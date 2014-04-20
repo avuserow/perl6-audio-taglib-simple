c9d227867b9c1eff00e3e6445d74b0783cbd0760
Adrian Kreher <avuserow@gmail.com>
2014-04-20 01:29:26 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,88 @@
+# NAME
+
+Audio::Taglib::Simple - Read ID3 and other audio metadata with TagLib
+
+# SYNOPSIS
+
+```perl6
+my $taglib = Audio::Taglib::Simple.new("awesome.mp3");
+say "artist: ", $taglib.artist;
+```
+
+See also examples/taglib.p6
+
+# DESCRIPTION
+
+This module uses NativeCall to provide bindings to TagLib's C API. The C API is
+the "simple" API, which only provides commonly used fields that are abstracted
+over all file types.
+
+TagLib supports many audio and tag formats. Audio formats include MP3, MPC,
+FLAC, MP4, ASF, AIFF, WAV, TrueAudio, WavPack, Ogg FLAC, Ogg Vorbis, Speex and
+Opus file formats. Tag formats include ID3v1, ID3v2, APE, FLAC, Xiph,
+iTunes-style MP4 and WMA. See their website for an exhaustive list.
+
+TagLib is nice and fast. The example script runs on a directory of 501 files
+(499 are MP3, 2 are non-audio) in around 4 seconds.
+
+All of the API methods are read only. Modifying a file is not yet supported.
+
+# METHODS
+
+## new($file)
+
+Prepares to read the provided file. Dies if the file does not exist or if
+TagLib cannot parse it. TagLib attempts to guess the file type here.
+
+## file
+
+Readonly accessor to the file variable passed in.
+
+## artist
+
+## album
+
+## title
+
+## comment
+
+## genre
+
+## year
+
+Returns the year associated with the song as an integer (e.g. 2004) or 0 if not
+present.
+
+## length
+
+Returns the length of the file as a Duration. TagLib provides it as an integer.
+
+## bitrate
+
+Returns the bit rate of the file as an Int in kb/s. Example: 128.
+
+## samplerate
+
+Returns the sample rate of the file as an Int in Hz. Example: 44100.
+
+## channels
+
+Returns the number of channels present in the file as an Int. Example: 2.
+
+# CAVEATS
+
+- TagLib prints some warnings to STDERR directly.
+- All fields are read from the metadata, and corrupted files can often have
+  incorrect values for length. TagLib does not actually parse the music stream
+  to find this out.
+
+# TODO
+
+See [TODO](TODO).
+
+# SEE ALSO
+
+[TagLib website](http://taglib.github.io)
+
+[tag\_c.h from the TagLib project](https://github.com/taglib/taglib/blob/master/bindings/c/tag_c.h)
+
---
+++ lib/Audio/Taglib/Simple.pm
@@ -0,0 +1,110 @@
+use v6;
+
+use NativeCall;
+
+class Audio::Taglib::Simple {
+	has $.file is readonly;
+	has $.taglib-file is readonly;
+	has $!taglib-tag;
+	has $!taglib-audio;
+
+	#= Open the provided file to read its tags.
+	method new(Str $file) {
+		die "File does not exist" unless $file.path ~~ :e;
+
+		my $taglib-file = taglib_file_new($file);
+		die "Invalid audio file" unless $taglib-file;
+		self.bless(:$file, :$taglib-file);
+	}
+
+	method !setup-file-tag() {
+		unless $!taglib-tag {
+			$!taglib-tag = taglib_file_tag($.taglib-file);
+		}
+	}
+
+	method title() returns Str {
+		self!setup-file-tag;
+		return taglib_tag_title($!taglib-tag);
+	}
+
+	method artist() returns Str {
+		self!setup-file-tag;
+		return taglib_tag_artist($!taglib-tag);
+	}
+
+	method album() returns Str {
+		self!setup-file-tag;
+		return taglib_tag_album($!taglib-tag);
+	}
+
+	method comment() returns Str {
+		self!setup-file-tag;
+		return taglib_tag_comment($!taglib-tag);
+	}
+
+	method genre() returns Str {
+		self!setup-file-tag;
+		return taglib_tag_genre($!taglib-tag);
+	}
+
+	method year() returns Int {
+		self!setup-file-tag;
+		return taglib_tag_year($!taglib-tag);
+	}
+
+	method track() returns Int {
+		self!setup-file-tag;
+		return taglib_tag_track($!taglib-tag);
+	}
+
+	method !setup-file-audio() {
+		unless $!taglib-audio {
+			$!taglib-audio = taglib_file_audioproperties($.taglib-file);
+		}
+	}
+
+	method length() returns Duration {
+		self!setup-file-audio;
+		return Duration.new(taglib_audioproperties_length($!taglib-audio));
+	}
+
+	method bitrate() returns Int {
+		self!setup-file-audio;
+		return taglib_audioproperties_bitrate($!taglib-audio);
+	}
+
+	method samplerate() returns Int {
+		self!setup-file-audio;
+		return taglib_audioproperties_samplerate($!taglib-audio);
+	}
+
+	method channels() returns Int {
+		self!setup-file-audio;
+		return taglib_audioproperties_channels($!taglib-audio);
+	}
+
+
+	# Routines we use automatically for the actual API calls
+	sub taglib_file_new(Str) returns OpaquePointer is native('libtag_c') {...};
+
+	# XXX use this someday when we can represent the enum of the types
+	#sub taglib_file_new_type(Str, Enum) returns OpaquePointer is native('libtag_c') {...};
+	sub taglib_file_tag(OpaquePointer) returns OpaquePointer is native('libtag_c') {...};
+	sub taglib_file_audioproperties(OpaquePointer) returns OpaquePointer is native('libtag_c') {...};
+
+	# Tag APIs
+	sub taglib_tag_title(OpaquePointer) returns Str is native('libtag_c') {...};
+	sub taglib_tag_artist(OpaquePointer) returns Str is native('libtag_c') {...};
+	sub taglib_tag_album(OpaquePointer) returns Str is native('libtag_c') {...};
+	sub taglib_tag_comment(OpaquePointer) returns Str is native('libtag_c') {...};
+	sub taglib_tag_genre(OpaquePointer) returns Str is native('libtag_c') {...};
+	sub taglib_tag_year(OpaquePointer) returns Int is native('libtag_c') {...};
+	sub taglib_tag_track(OpaquePointer) returns Int is native('libtag_c') {...};
+
+	# Audio Properties APIs
+	sub taglib_audioproperties_length(OpaquePointer) returns Int is native('libtag_c') {...};
+	sub taglib_audioproperties_bitrate(OpaquePointer) returns Int is native('libtag_c') {...};
+	sub taglib_audioproperties_samplerate(OpaquePointer) returns Int is native('libtag_c') {...};
+	sub taglib_audioproperties_channels(OpaquePointer) returns Int is native('libtag_c') {...};
+}
